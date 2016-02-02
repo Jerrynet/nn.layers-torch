fe726241f7d178e8816c9843754ffbf529ca44e0
Jerry Lin <jerry.net@gmail.com>
2016-02-02 15:13:00 +0800

    Upload first few util layers

---
+++ utils/RemoveLastSingletonDim.lua
@@ -0,0 +1,29 @@
+-- RemoveLastSingletonDim
+-- if the last dimension is 1, remove it
+
+local RemoveLastSingletonDim, Parent = torch.class('nn.RemoveLastSingletonDim', 'nn.Module')
+
+function RemoveLastSingletonDim:__init()
+    Parent.__init(self)
+    self.size = -1
+    self.didRemove = false
+end
+
+function RemoveLastSingletonDim:updateOutput(input)
+    self.size = input:size():totable()
+    if self.size[#self.size] == 1 then
+        self.size[#self.size] = nil
+        self.didRemove = true
+    end
+    self.output = input:view(table.unpack(self.size))
+    return self.output
+end
+
+function RemoveLastSingletonDim:updateGradInput(input, gradOutput)
+    if self.didRemove then
+        self.size[#self.size+1] = 1
+    end
+    self.gradInput = gradOutput:view(table.unpack(self.size))
+    self.didRemove = false
+    return self.gradInput
+end
---
+++ utils/ViewAdv.lua
@@ -0,0 +1,58 @@
+-- ViewAdv
+-- Advanced View() layer for same dimension transform
+--
+-- Usage
+-- layer = nn.ViewAdv(dim1_size, dim2_size, ...)
+-- note: argument number must be the same as input dimension number
+--
+-- dimX_size: 0 = same as input, -1 = Unknown
+--
+-- Example
+-- a = torch.FloatTensor({{{1,2,3},{4,5,6}},{{1,2,3},{4,5,6}}}) -- size 2x2x3
+-- layer = nn.ViewAdv(1, -1, 0)
+-- -- outputs size = 1x4x3
+
+local ViewAdv, parent = torch.class('nn.ViewAdv', 'nn.Module')
+
+function ViewAdv:__init(...)
+    parent.__init(self)
+    self.size = {...}
+    self.input_size = {}
+    self.performSize = {}
+end
+
+function ViewAdv:updateOutput(input)
+    local nEle = input:nElement()
+    self.input_size  = input:size():totable()
+    self.performSize = input:size():totable()
+    assert(#self.input_size==#self.size)
+
+    local isOneMinusone = 0
+    local minusOneLoc = 0
+    local currentEle = 1
+
+    for i=1,#self.size do
+        if self.size[i] == 0 then
+            self.performSize[i] = self.input_size[i]
+            currentEle = currentEle*self.input_size[i]
+        elseif self.size[i] == -1 then
+            isOneMinusone = isOneMinusone+1
+            minusOneLoc = i
+            if (isOneMinusone>1) then
+                error('Should have only one -1 in nn.ViewAdv arguments.')
+            end
+        elseif self.size[i] > 0 then
+            self.performSize[i] = self.size[i]
+            currentEle = currentEle*self.performSize[i]
+        end
+    end
+
+    self.performSize[minusOneLoc] = nEle/currentEle
+    self.output = input:view(table.unpack(self.performSize))
+    return self.output
+end
+
+function ViewAdv:updateGradInput(input, gradOutput)
+    self.gradInput = gradOutput:view(table.unpack(self.input_size))
+    return self.gradInput
+end
